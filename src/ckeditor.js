d67152bbcb21762459b2d39309c4856bf350a901
SeungMin <tmdals9974@naver.com>
2021-04-27 14:25:54 +0900

    #2021. 04. 27

---
+++ src/ckeditor.js
@@ -0,0 +1,147 @@
+/**
+ * @license Copyright (c) 2014-2021, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor.js";
+import Alignment from "@ckeditor/ckeditor5-alignment/src/alignment.js";
+import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote.js";
+import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold.js";
+import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials.js";
+import FontBackgroundColor from "@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js";
+import FontColor from "@ckeditor/ckeditor5-font/src/fontcolor.js";
+import FontFamily from "@ckeditor/ckeditor5-font/src/fontfamily.js";
+import FontSize from "@ckeditor/ckeditor5-font/src/fontsize.js";
+import Heading from "@ckeditor/ckeditor5-heading/src/heading.js";
+import Highlight from "@ckeditor/ckeditor5-highlight/src/highlight.js";
+import HorizontalLine from "@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js";
+import Image from "@ckeditor/ckeditor5-image/src/image.js";
+import ImageCaption from "@ckeditor/ckeditor5-image/src/imagecaption.js";
+import ImageResize from "@ckeditor/ckeditor5-image/src/imageresize.js";
+import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle.js";
+import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar.js";
+import ImageUpload from "@ckeditor/ckeditor5-image/src/imageupload.js";
+import Indent from "@ckeditor/ckeditor5-indent/src/indent.js";
+import IndentBlock from "@ckeditor/ckeditor5-indent/src/indentblock.js";
+import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic.js";
+import Link from "@ckeditor/ckeditor5-link/src/link.js";
+import LinkImage from "@ckeditor/ckeditor5-link/src/linkimage.js";
+import List from "@ckeditor/ckeditor5-list/src/list.js";
+import ListStyle from "@ckeditor/ckeditor5-list/src/liststyle.js";
+import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph.js";
+import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice";
+import SimpleUploadAdapter from "@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter.js";
+import SpecialCharacters from "@ckeditor/ckeditor5-special-characters/src/specialcharacters.js";
+import SpecialCharactersArrows from "@ckeditor/ckeditor5-special-characters/src/specialcharactersarrows.js";
+import SpecialCharactersCurrency from "@ckeditor/ckeditor5-special-characters/src/specialcharacterscurrency.js";
+import SpecialCharactersEssentials from "@ckeditor/ckeditor5-special-characters/src/specialcharactersessentials.js";
+import SpecialCharactersText from "@ckeditor/ckeditor5-special-characters/src/specialcharacterstext.js";
+import Strikethrough from "@ckeditor/ckeditor5-basic-styles/src/strikethrough.js";
+import Table from "@ckeditor/ckeditor5-table/src/table.js";
+import TableCellProperties from "@ckeditor/ckeditor5-table/src/tablecellproperties";
+import TableProperties from "@ckeditor/ckeditor5-table/src/tableproperties";
+import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar.js";
+import Underline from "@ckeditor/ckeditor5-basic-styles/src/underline.js";
+
+class Editor extends ClassicEditor {}
+
+// Plugins to include in the build.
+Editor.builtinPlugins = [
+  Alignment,
+  BlockQuote,
+  Bold,
+  Essentials,
+  FontBackgroundColor,
+  FontColor,
+  FontFamily,
+  FontSize,
+  Heading,
+  Highlight,
+  HorizontalLine,
+  Image,
+  ImageCaption,
+  ImageResize,
+  ImageStyle,
+  ImageToolbar,
+  ImageUpload,
+  Indent,
+  IndentBlock,
+  Italic,
+  Link,
+  LinkImage,
+  List,
+  ListStyle,
+  Paragraph,
+  PasteFromOffice,
+  SimpleUploadAdapter,
+  SpecialCharacters,
+  SpecialCharactersArrows,
+  SpecialCharactersCurrency,
+  SpecialCharactersEssentials,
+  SpecialCharactersText,
+  Strikethrough,
+  Table,
+  TableCellProperties,
+  TableProperties,
+  TableToolbar,
+  Underline,
+];
+
+Editor.defaultConfig = {
+  toolbar: [
+    "heading",
+    "|",
+    "fontFamily",
+    "fontColor",
+    "fontSize",
+    "|",
+    "bold",
+    "underline",
+    "strikethrough",
+    "italic",
+    "highlight",
+    "fontBackgroundColor",
+    "|",
+    "alignment",
+    "bulletedList",
+    "numberedList",
+    "outdent",
+    "indent",
+    "|",
+    "blockQuote",
+    "insertTable",
+    "specialCharacters",
+    "link",
+    "horizontalLine",
+    "imageUpload",
+    "|",
+    "undo",
+    "redo",
+  ],
+  language: "ko",
+  image: {
+    toolbar: [
+      "imageTextAlternative",
+      "imageStyle:alignLeft",
+      "imageStyle:full",
+      "imageStyle:alignRight",
+      "linkImage",
+    ],
+    styles: [
+      'full',
+      'alignLeft',
+      'alignRight'
+    ]
+  },
+  table: {
+    contentToolbar: [
+      "tableColumn",
+      "tableRow",
+      "mergeTableCells",
+      "tableCellProperties",
+      "tableProperties",
+    ],
+  },
+  licenseKey: "",
+};
+
+export default Editor;
